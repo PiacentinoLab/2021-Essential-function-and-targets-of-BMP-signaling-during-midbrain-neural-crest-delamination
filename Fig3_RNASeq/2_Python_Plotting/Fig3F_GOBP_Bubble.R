60936f4bbcd5bf73c32d98153c1fa467f2ea91f9
Mike Piacentino <mpiacent@caltech.edu>
2021-03-27 09:50:57 -0700

    Fig3 Code Initial Upload

---
+++ Fig3_RNASeq/2_Python_Plotting/Fig3F_GOBP_Bubble.R
@@ -0,0 +1,40 @@
+library(datasets)
+library(ggplot2)
+library(multcomp)
+library(nlme)
+library(XLConnect)
+library(XLConnectJars)
+library(lme4)
+library(rJava)
+library(xlsx)
+library(readxl)
+
+getwd()
+#setwd("/Users/ivanferreira/Downloads/")
+#dev.copy2pdf(file="GO_terms_allabove_3_refined.pdf")
+aq_trim1 <- read.csv('../Python/PANTHER_Analysis/BubblePlot_Data.csv')
+aq_trim1 <- aq_trim1[which(aq_trim1$GeneSet == "Downregulated" |
+                            aq_trim1$GeneSet == "Upregulated"), ]
+head(aq_trim1)
+
+aq_trim1$GeneSet <- factor(aq_trim1$GeneSet, labels = c("Downregulated", "Upregulated")) 
+
+setEPS()
+
+postscript("GO_Biological_Process.eps")
+p7 <- ggplot(aq_trim1, aes(x = GeneSet, y = reorder(GOBP,-pvalue), 
+                size = FoldEnrichment, fill = pvalue)) +
+  geom_point(shape = 21) +
+  theme_bw() +
+  theme()
+p7 <-  p7 + scale_fill_continuous(low = "red", high = "white")
+#p7 <-  p7 + scale_fill_continuous(type="viridis", direction=-1)
+p7 <- p7 + 
+  #ggtitle("Lipid metabolic gene enrichment") +
+  labs(x = "GeneSet", y = "Biological Process") + 
+  scale_size(range = c(1, 15)) +
+  theme(legend.position="right", legend.direction="vertical",
+        legend.box = "vertical",
+        legend.key.size = unit(0.6, "cm"))
+p7
+dev.off()
